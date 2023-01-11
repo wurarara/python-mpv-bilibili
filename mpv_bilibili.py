e6c215de13e6d36b2cfd616153b2886d5b74253b
warara <73343292+wurarara@users.noreply.github.com>
2023-01-11 19:12:52 +0800

    Update mpv_bilibili.py

--- mpv_bilibili.py
+++ mpv_bilibili.py
@@ -4,12 +4,12 @@ from you_get import common as you_get                #导入you-get库
 
 cookies_bl = "Enter the path of the cookies.txt exported from bilibili between the quotation marks"   #在引号之间输入从bilibili导出的cookies.txt的路径
 sys.argv = ['you-get','-c',cookies_bl]
-video_url = input("input URL: ")
-url = video_url     #需要下载的视频地址
+video_url = input("input URL: ")        #输入视频页链接
+url = video_url
 sys.argv = ['you-get', '-i', url,'--no-proxy']          #获取视频信息，且不使用代理
 you_get.main()
 
-format_msg = input("input format_msg: ")            #选择format，并输入
+format_msg = input("input format_msg: ")            #选择format输入
 sys.argv = ['you-get','--format={}'.format(format_msg),'-u',url,'--no-proxy']        #获取你选择的视频格式的真实链接，且不使用代理
 you_get.main()
 
